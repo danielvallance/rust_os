ae2509962928e9ef9d321aa10a32514845ea1dff
danielvallance <danielvallance3435@gmail.com>
2025-10-04 20:02:41 +0100

    Bare-minimum freestanding executable
    This commit provides a trivial panic handler,
    entry point, and instructs the linker to avoid
    linking to the standard library.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Small Operating System Written in Rust
+
+I am reading a blog which contains a series of tutorials on writing a small operating system in Rust (https://os.phil-opp.com/). This repository will contain the code I write as I follow along.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+//! This is a bare-minimum freestanding Rust executable
+
+// #![no_main] tells rustc that we do not want to use the entry point defined by the
+// Rust runtime (as the Rust runtime requires an underlying OS).
+#![no_main]
+// #![no_std] tells rustc that we do not want to link this executable against the
+// standard library (as it relies on an underlying OS).
+#![no_std]
+
+use core::panic::PanicInfo;
+
+/// This is a custom panic handler, as we do not have access to the default
+/// one in the standard library. This panic handler just loops forever.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+/// The '#[unsafe(no_mangle)]' attribute directs rustc to not mangle the name,
+/// as we need to pass the name of this entry point function to the linker.
+///
+/// We also specify that it uses the C calling convention as this
+/// executable will be called with the C calling convention, not the
+/// Rust one. This is because this freestanding executable will not
+/// be invoked by the Rust runtime.
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
