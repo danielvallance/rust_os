c44263005aaa37506c5e1571ef197a2bb40d7281
danielvallance <danielvallance3435@gmail.com>
2025-10-05 17:18:01 +0100

    Bare minimum kernel and bootable image
    This commit creates a custom target defined in
    "x64_64-rust_os.json" which is suitable for compiling
    our kernel against.
    
    Notably, it disables SIMD features (as the context switching
    on interrupt is expensive when using SIMD registers), enables
    using non-SIMD registers for floating point operations, disables
    temporary usage of memory beyond the stack pointer (as it could
    result in corruptions when it is overwritten by interrupt routines)
    and it uses the cross-platform LLD linker.
    
    The bare-minimum freestanding binary now writes
    "Hello world!" into the VGA buffer.
    
    When "cargo run" is called, the bare minimum kernel is compiled,
    the "bootimage" tool links the resulting executable to the
    bootloader dependency to create a bootable image which is
    run in QEMU.

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,9 @@
 
 use core::panic::PanicInfo;
 
+static HELLO_WORLD: &[u8] = b"Hello world!";
+const VGA_BUF_ADDR: *mut u8 = 0xb8000 as *mut u8;
+
 /// This is a custom panic handler, as we do not have access to the default
 /// one in the standard library. This panic handler just loops forever.
 #[panic_handler]
@@ -25,5 +28,13 @@ fn panic(_info: &PanicInfo) -> ! {
 /// be invoked by the Rust runtime.
 #[unsafe(no_mangle)]
 pub extern "C" fn _start() -> ! {
+    // Write ASCII byte and colour byte into VGA buffer
+    for (i, &byte) in HELLO_WORLD.iter().enumerate() {
+        unsafe {
+            *VGA_BUF_ADDR.offset(i as isize * 2) = byte;
+            *VGA_BUF_ADDR.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
